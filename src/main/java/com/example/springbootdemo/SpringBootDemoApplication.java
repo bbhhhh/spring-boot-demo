b6bb8c56bb8d5ce04cd72cab838940aa9e0fb247
Bian <haihua.bian@nokia-sbell.com>
2022-10-20 16:38:15 +0800

    add new files

--- src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/example/springbootdemo/SpringBootDemoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.example.springbootdemo.jms.FirstAMQConsumer;
 
 @SpringBootApplication
+@EnableWebMvc
 public class SpringBootDemoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/springbootdemo/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.example.springbootdemo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Project: spring-boot-demo
+ * @Author : haihuab
+ * @Date : 2022/10/11 16:56
+ * @Description:
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
